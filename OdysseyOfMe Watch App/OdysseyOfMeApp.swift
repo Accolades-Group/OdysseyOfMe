ecb042631ac3138e71b973a024a788f3775c6757
Tanner Brown <tbrown@accolades.group>
2022-11-03 15:59:49 -0700

    Initial Commit

---
+++ OdysseyOfMe Watch App/OdysseyOfMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyOfMeApp.swift
+//  OdysseyOfMe Watch App
+//
+//  Created by Tanner Brown on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct OdysseyOfMe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OdysseyOfMe/OdysseyOfMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyOfMeApp.swift
+//  OdysseyOfMe
+//
+//  Created by Tanner Brown on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct OdysseyOfMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
